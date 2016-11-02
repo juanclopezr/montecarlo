65cdb9aa4a0d03a0e20babc86a207d7e9a4f161b
Juan Lopez <jc.lopez11@uniandes.edu.co>
2016-11-02 13:30:09 -0500

    generation of gaussian distribution

--- randomgen.py
+++ randomgen.py
@@ -4,15 +4,21 @@ import numpy as np
 m = 123346
 n = 8121
 k = 28411
+Ns = 1000
 N = 10000
 
 idum = 5
 rands = np.array([])
 
-for i in range(N):
-    idum = (idum*n+k)%m
-    ran = float(idum)/float(m)
-    rands = np.append(rands,[ran])
+for j in range(N):
+    randi = 0
+    for i in range(Ns):
+        idum = (idum*n+k)%m
+        ran = float(idum)/float(m)-0.5
+        randi += ran
+    rands = np.append(rands,[randi/1000**0.5])
 
-plt.hist(rands)
+plt.hist(rands,bins=50)
 plt.show()
+
+
